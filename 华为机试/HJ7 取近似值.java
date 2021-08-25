72cc165534ddb3a553812662b9066ac1f16a4d3d
AaronPhantomhive <44737094+AaronPhantomhive@users.noreply.github.com>
2021-08-25 16:47:31 +0800

    Update HJ7 取近似值.java

--- 华为机试/HJ7 取近似值.java
+++ 华为机试/HJ7 取近似值.java
@@ -1,1 +1,10 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String arg[]) {
+        Scanner scanner = new Scanner(System.in);
+        float f = scanner.nextFloat();
+        int result = (int)Math.round(f);
+        System.out.println(result);
+    }
+}
