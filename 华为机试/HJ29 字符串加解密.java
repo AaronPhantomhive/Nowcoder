6eda730396710f8bd829ec70e5c547279b78a74e
AaronPhantomhive <44737094+AaronPhantomhive@users.noreply.github.com>
2021-08-29 22:14:01 +0800

    Create HJ29 字符串加解密.java

---
+++ 华为机试/HJ29 字符串加解密.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String before = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        String after = "BCDEFGHIJKLMNOPQRSTUVWXYZAbcdefghijklmnopqrstuvwxyza1234567890";
+        while(sc.hasNextLine()){
+            String s1 = sc.nextLine();
+            String s2 = sc.nextLine();
+            for(int i = 0;i<s1.length();i++){
+                System.out.print(after.charAt(before.indexOf(s1.charAt(i))));
+            }
+            System.out.println();
+            for(int i = 0;i<s2.length();i++){
+                System.out.print(before.charAt(after.indexOf(s2.charAt(i))));
+            }
+            System.out.println();
+        }
+    }
+}
